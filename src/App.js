7371910eee2601e0bf37e62e1c6f995d02574b48
Kisalka Gunthilaka <gunathilakakisalka@gmail.com>
2022-05-07 18:02:46 +0530

    Add receiver account

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import { bindActionCreators } from 'redux';
 import { actionCreators } from './state/index';
 
 function App() {
-  const account = useSelector((state) => state.account);
+  const account = useSelector((state) => state.account.account);
+  const receivee = useSelector((state) => state.account.receiveeAccountBalance);
+
   const dispatch = useDispatch()
 
   const { depositMoney, withdrawMoney, transferMoney} = bindActionCreators(actionCreators, dispatch);
   
   return (
     <div className="App">
-      <h1> {account} </h1>
+      <h1> Account balance : {account} </h1>
+      <h1> Receiver Account balance : {receivee} </h1>
       <button onClick={() => depositMoney(1000)}> Deposit </button>
       <button onClick={() => withdrawMoney(1000)}> Withdraw </button>
       <button onClick={() => transferMoney(500)}>Transfer</button>
--- src/state/reducers/accountReducer.js
+++ src/state/reducers/accountReducer.js
@@ -1,12 +1,23 @@
-const reducer = (state = 0 , action) => {
+const initialState = {account :0,receiveeAccountBalance:0}
+
+const reducer = (state = initialState , action) => {
   switch (action.type) {
-      case "deposit":
-          return state + action.payload;
+      case "deposit":        
+          return {
+            ...state,
+            account : state.account + action.payload
+          }
       case "withdraw":
-          return state - action.payload; 
-      case "transfer":
-          return state + action.payload;      
-          
+          return {
+            ...state,
+             account : state.account - action.payload
+          }
+      case "transfer":      
+           return {
+            ...state,
+            account: state.account - action.payload,
+            receiveeAccountBalance: state.receiveeAccountBalance + action.payload
+        }          
           default:
                return state    
   }
